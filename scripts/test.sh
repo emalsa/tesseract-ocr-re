3a7dc0011e11d13aa474d644715f4aaed4da3f8b
dni <daniele@nicastro.io>
2022-09-13 20:26:04 +0200

    bitbucket to github

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+for i in 1 2 3
+do
 docker ps -f name=t4re
 TASK_TMP_DIR=TASK_$$_$(date +"%N")
 echo "====== TASK $TASK_TMP_DIR started ======"
 docker exec -it t4re mkdir \-p ./$TASK_TMP_DIR/
-docker cp ./ocr-files/phototest.tif t4re:/home/work/$TASK_TMP_DIR/
-docker exec -it t4re /bin/bash -c "mkdir -p ./$TASK_TMP_DIR/out/; cd ./$TASK_TMP_DIR/out/; tesseract ../phototest.tif phototest -l eng --psm 1 --oem 3 txt pdf hocr"
+
+echo "Start $i.png"
+docker cp ./ocr-files/$i.png t4re:/home/work/$TASK_TMP_DIR/
+#docker cp ./ocr-files/1400.png t4re:/home/work/$TASK_TMP_DIR/
+
+docker exec -it t4re /bin/bash -c "mkdir -p ./$TASK_TMP_DIR/out/; cd ./$TASK_TMP_DIR/out/; tesseract ../$i.png $i -l deu --psm 1 --oem 3 pdf hocr"
 mkdir -p ./ocr-files/output/$TASK_TMP_DIR/
 docker cp t4re:/home/work/$TASK_TMP_DIR/out/ ./ocr-files/output/$TASK_TMP_DIR/
 docker exec -it t4re rm \-r ./$TASK_TMP_DIR/
 docker exec -it t4re ls
+echo "Done $i.png"
 echo "====== Result files was copied to ./ocr-files/output/$TASK_TMP_DIR/ ======"
+done
